762be3cfe654da70f7cc6981527ad050b07a2dcc
Everton Pavan <everton.pavan@sertao.ifrs.edu.br>
2021-04-16 18:07:41 -0300

    feat: add database queries

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,31 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const games =  await this.repository
+      .createQueryBuilder('games')
+      .where("games.title ILIKE :title", { title: `%${param}%` })
+      .getMany();
+
+    return games 
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    const count =  this.repository.query(
+      `
+      SELECT COUNT(*) FROM GAMES
+      `
+    ); 
+
+    return count
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
+    const users =  this.repository
       .createQueryBuilder()
-      // Complete usando query builder
+      .relation(Game, "users")
+      .of(id)
+      .loadMany()
+
+      return users
   }
 }
